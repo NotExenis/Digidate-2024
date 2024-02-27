bbbc805804b585abdd3a614840923f7d0e968e95
Robin Oude Luttikhuis <00277188@rijnijssel.nl>
2024-02-27 09:08:13 +0100

    Navbar

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -8,12 +8,12 @@ if(isset($_SESSION['user_role'])){
         $navitems = array(
             array('test', 'test'),
         );
-    } else {
-        $navitems = array(
-            array('login', 'Login'),
-            array('register', 'Register'),
-        );
-    }
+    } 
+} else {
+    $navitems = array(
+        array('login', 'Login'),
+        array('register', 'Register'),
+    );
 }
 
 
