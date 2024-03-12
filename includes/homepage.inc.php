6648115ebadcb18ff35a366908ef2cb6adc607b3
Robin Oude Luttikhuis <00277188@rijnijssel.nl>
2024-03-12 08:59:31 +0100

    homepage

---
+++ includes/homepage.inc.php
@@ -0,0 +1,27 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+        </div>
+        <div class="col-sm">
+            <br>
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="photos/33945736.5f6fc770.160x160o.b46650643ee9.jpeg" alt="Levi">
+                <div class="card-body">
+                    <h5 class="card-title">Digidate</h5>
+                    <p class="card-text">Welcome to Digidate find your lover now!</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Filter through tags</li>
+                    <li class="list-group-item">Filter age and gender</li>
+                    <li class="list-group-item">Edit your account the way you like it</li>
+                </ul>
+                <div class="card-body">
+                    <a href="index.php?page=login" class="card-link">Click to sign in!</a>
+                    <a href="index.php?page=register" class="card-link">Click to register your account!</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+        </div>
+    </div>
+</div>
