d6dcb9bbf61ebca7727acd09639aeec6b177d782
Exoo, Thijn - Student Rijn IJssel <00303915@rijnijssel.nl>
2024-03-12 14:58:03 +0100

    done

--- functions/popupmessage.php
+++ functions/popupmessage.php
@@ -1,25 +1,39 @@
 <?php
-function popupmessage($message){
+function popupmessage($title, $message, $buttontext){
     ?>
-    <div class="modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+    <script>
+        $(document).ready(function() {
+            $('#close').on('click', function() {
+                $('#popup').hide();
+            });
+            $('#cancel').on('click', function() {
+                $('#popup').hide();
+            });
+        });
+    </script>
+
+    <div id="popup" class="container position-absolute top-10 start-50 translate-middle-x">
+    <div class="row">
+    <div class="col-sm"></div>
+    <div class="col-sm">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                <h5 class="card-title"><?php echo $title ?></h5>
+                    <button type="button" id="close" class="btn btn-close pull-right" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <?php echo $message ?>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <hr>
+                <p class="card-text"><?php echo $message ?></p>
+                <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-primary m-1"><?php echo $buttontext ?></button>
+                    <button type="submit" id="cancel" class="btn btn-danger m-1">Cancel</button>
                 </div>
             </div>
         </div>
     </div>
+    <div class="col-sm"></div>
+    </div>
+</div>
 
     <?php
 }
