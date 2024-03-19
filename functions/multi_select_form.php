7a98adbeb30ab48101f4d7ec1c209eff0a0ebb86
Meulen van der, Jan Paul - Student Rijn IJssel <00253791@rijnijssel.nl>
2024-03-19 16:21:42 +0100

    profile edit aan het werken. foto en description werkt al.

--- functions/multi_select_form.php
+++ functions/multi_select_form.php
@@ -1,18 +1,23 @@
 <?php
 
-function multiForm($table) {
+function multiFormTags($table) {
 
     include "private/conn.php";
 
-    try {
-        $sql = "SELECT * FROM " . $table . " ";
-        $stmt = $db->prepare($sql);
-        $stmt->execute();
-    }  catch (Exception $e) {
-
-    }
+    $sql = "SELECT * FROM " . $table . " ";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+    try {
+        $sql2 = "SELECT * FROM tbl_usertags WHERE usertags_users_id = :user_id";
+        $stmt2 = $db->prepare($sql2);
+        $stmt2->bindParam(":user_id", $_SESSION['users_id']);
+        $stmt2->execute();
+        $user_tags = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+        echo $e;
+    }
     ?>
     <script>
         $(document).ready(function() {
@@ -20,24 +25,38 @@ function multiForm($table) {
                 $('#error').hide();
             });
         });
+            $(document).ready(function(){
+            $('.badge-clickable').click(function(){
+                $('#checkbox').toggle('checked');
+            });
+        });
+
     </script>
     <div class="" id="error" role="alert">
-        <div class="d-flex justify-content-between">
+        <div class="container">
             <div>search</div>
-            <br>
-            <?php
-                foreach($result as $array) {
-                    foreach($array as $key => $value) {
-                        $prefix = strstr($key, '_', false);
-                        ?>
-                    <div class="">
 
-                        <label><?= $prefix ?></label>
-                        <label><?=$value?></label>
-                    </div>
-                        <br>
             <?php
-                    }
+
+                foreach($result as $array) {
+                        $color = isset($array["tags_color"]) ? $array["tags_color"] : "blue";
+
+
+                        if(in_array($user_tags['usertags_tags_id'], $array)) {
+                        }
+
+                        ?>
+                    <div class="container-sm">
+                            <?php if(in_array($array['tags_id'], $user_tags)) { ?>
+                        <span class="badge rounded-fill badge-clickable" style="background-color: <?= $color ?>"><?= $array['tags_title'] ?>
+                            <input class="checkbox" type="checkbox" checked>
+                        </span> <?php } else { ?>
+                        <span class="badge rounded-fill badge-clickable" style="background-color: <?= $color ?>"><?= $array['tags_title'] ?>
+                            <input class="checkbox" type="checkbox">
+                        </span> <?php } ?>
+                    </div>
+            <?php
+
                 }
              ?>
 
--- includes/profile_users_edit.inc.php
+++ includes/profile_users_edit.inc.php
@@ -6,6 +6,8 @@ if (isset($_SESSION['notification'])) {
     unset($_SESSION['notification']);
 }
 
+
+
 $sql_location = "SELECT municipality_name, municipality_id
                  FROM tbl_municipalities";
 $stmt_location = $db->prepare($sql_location);
@@ -21,15 +23,34 @@ $sql_tags = "SELECT tags_title, tags_id
 $stmt_tags = $db->prepare($sql_tags);
 $stmt_tags->execute();
 
+$sql_images = "SELECT * FROM tbl_images WHERE images_user_id = :user_id";
+$stmt_images = $db->prepare($sql_images);
+$stmt_images->bindParam(":user_id", $_SESSION['users_id']);
+$stmt_images->execute();
+
+$sql_user_education = "SELECT education_name FROM tbl_users INNER JOIN tbl_users_education ON users_education_users_id = users_id INNER JOIN tbl_education ON education_id = users_education_id WHERE users_id = :user_id";
+$stmt_user_education = $db->prepare($sql_user_education);
+$stmt_user_education->bindParam(":user_id", $_SESSION['users_id']);
+$stmt_user_education->execute();
 
 ?>
+<script>
+    $(document).ready(function() {
+        $("#image").on("change", function() {
+            if ($("#image")[0].files.length > 5) {
+                alert("You can select only 5 images");
+                window.location.href = "http://localhost/digidate/index.php?page=profile_users_edit";
+            }
+        });
+    });
+</script>
 <div class="container">
     <div class="col-sm">
     </div>
     <div class="col-sm">
 
 <?php
-multiForm('tbl_tags');
+multiFormTags('tbl_tags');
 ?>
 
     </div>
@@ -44,14 +65,14 @@ multiForm('tbl_tags');
 
         </div>
         <div class="col-sm">
-            <form method="POST" action="">
+            <form method="POST" enctype="multipart/form-data" action="php/photo_upload.php">
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" name="email" placeholder="Enter email" required>
+                    <input type="email" class="form-control" name="email" placeholder="Enter email" >
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
+                        <input type="password" class="form-control" id="password" name="password" >
                 </div>
                 <div class="mb-3">
                     <label for="location" class="form-label">Location</label>
@@ -61,11 +82,24 @@ multiForm('tbl_tags');
                         <?php } ?>
                     </select>
                 </div>
+                <?php //fix dit nog aub ?>
                 <div class="mb-3">
                     <label for="education" class="form-label">Education</label>
-                    <select class="form-select"  value="<?= $education['education_id']?>" name="education" >
-                        <?php foreach($stmt_education as $education){ ?>
-                            <option><?= $education['education_name']?></option>
+                    <select class="form-select" name="education" >
+                        <?php foreach($stmt_education as $education){
+
+                            if($education['education_name'] == $stmt_user_education->fetch()) {
+                                ?>
+                                <option disabled><?= $education['education_name']?></option>
+
+                                <?php
+                            } else {
+                                ?>
+                                <option><?= $education['education_name']?></option>
+
+                                <?php
+                            }
+                            ?>
                         <?php } ?>
                     </select>
                 </div>
@@ -73,10 +107,20 @@ multiForm('tbl_tags');
                     <label for="education" class="form-label">Tags</label>
                     <select class="form-select" value="<?= $tags['tags_id']?>" name="tag[]" multiple>
                         <?php foreach($stmt_tags as $tags){ ?>
-                            <option><?= $tags['tags_title']?></option>
+                            <option><?= $tags['tags_title'] ?></option>
                         <?php } ?>
                     </select>
                 </div>
+                <div class="form-group mb-3 mt-3">
+                    <label>Select Image File:</label>
+                    Upload this file: <input type=file name="image[]" id="image" multiple="multiple" accept="image/jpeg, image/jpg, image/png">
+                </div>
+                <?php foreach ($stmt_images->fetchAll(PDO::FETCH_ASSOC) as $image) { ?>
+                    <img src="data:image/jpeg;base64, <?= $image['images_image'] ?> " style="max-width: 200px" />
+                <?php
+                }
+                 ?>
+
                 <br>
                 <button type="submit" class="btn btn-primary">Login</button>
             </form>
