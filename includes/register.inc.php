c4e2e12798027d8aeed5db5f14acef961091b2da
Meulen van der, Jan Paul - Student Rijn IJssel <00253791@rijnijssel.nl>
2024-04-09 10:14:40 +0200

    Matches bekijken klaar.

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -1,11 +1,13 @@
-<?php 
+<?php
 require 'private/conn.php';
 
-$sql_gender = "SELECT * FROM tbl_genders";
+$sql_gender = "SELECT genders_name 
+               FROM tbl_genders";
 $stmt_gender = $db->prepare($sql_gender);
 $stmt_gender->execute();
 
-$sql_location = "SELECT * FROM tbl_municipalities";
+$sql_location = "SELECT municipality_name 
+                 FROM tbl_municipalities";
 $stmt_location = $db->prepare($sql_location);
 $stmt_location->execute();
 ?>
@@ -14,7 +16,7 @@ $stmt_location->execute();
 <div class="container">
     <div class="row">
         <div class="col-sm">
-        <?php
+            <?php
             if (isset($_SESSION['notification'])) {
                 echo '<p class="text-red-500">' . $_SESSION['notification'] . '</p>';
                 unset($_SESSION['notification']);
@@ -57,24 +59,28 @@ $stmt_location->execute();
 
                     <div class="mb-3">
                         <label for="password" class="form-label">Password *</label>
-                        <input type="password" class="form-control" id="password" title="Password must contain at least 15 characters, including at least one digit, one lowercase letter, one uppercase letter, and one special character." pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()-_=+{};:,<.>]).{15,}$" name="password" required>
+                        <input type="password" class="form-control" id="password"
+                               title="Password must contain at least 15 characters, including at least one digit, one lowercase letter, one uppercase letter, and one special character."
+                               pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()-_=+{};:,<.>]).{15,}$"
+                               name="password" required>
                         <input type="checkbox" id="check">Show Password
                     </div>
 
                     <div class="mb-3">
                         <label for="gender" class="form-label">Gender *</label>
-                        <select class="form-select" name="gender" required>
-                            <?php foreach($stmt_gender as $gender){ ?>
-                                <option value="<?= $gender['genders_id']?>"><?= $gender['genders_name']?></option>
+                        <select class="form-select" value="<?= $gender['genders_id'] ?>" name="gender" required>
+                            <?php foreach ($stmt_gender as $gender) { ?>
+                                <option><?= $gender['genders_name'] ?></option>
                             <?php } ?>
                         </select>
                     </div>
 
                     <div class="mb-3">
                         <label for="location" class="form-label">Location *</label>
-                        <select class="form-select"  name="location" required>
-                            <?php foreach($stmt_location as $location){ ?>
-                                <option  value="<?= $location['municipality_id']?>"><?= $location['municipality_name']?></option>
+                        <select class="form-select" value="<?= $location['municipality_id'] ?>" name="location"
+                                required>
+                            <?php foreach ($stmt_location as $location) { ?>
+                                <option><?= $location['municipality_name'] ?></option>
                             <?php } ?>
                         </select>
                     </div>
@@ -86,17 +92,20 @@ $stmt_location->execute();
 
                     <div class="mb-3">
                         <label for="age" class="form-label">Date of Birth *</label>
-                        <input type="date" class="form-control" id="age" name="age" max="<?php echo date('Y-m-d', strtotime('-18 years')); ?>" required>
+                        <input type="date" class="form-control" id="age" name="age"
+                               max="<?php echo date('Y-m-d', strtotime('-18 years')); ?>" required>
                     </div>
 
                     <button type="submit" class="btn btn-primary">Register</button>
                 </form>
             </div>
-
-        </div>
-        <div class="col-sm">
         </div>
     </div>
+</div>
+</div>
+<div class="col-sm">
+</div>
+</div>
 </div>
 
 
